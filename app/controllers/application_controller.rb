127215779aed0133964f8f4bb2de22d5ae1d19ca
Israel <chizunguisrael@gmail.com>
2023-05-20 20:17:49 +0200

    create controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :set_current_user
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :encrypted_password)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
+  private
+
+  def set_current_user
+    @current_user = current_user
+  end
 end
